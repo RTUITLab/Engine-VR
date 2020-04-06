8e439b9b88ce76edbf6cdf341b3b56e3a1711aa6
Mixail <mixail.korchikov@mail.ru>
2020-04-06 22:44:03 +0300

    NeedTest

---
+++ Assets/Scripts/Online/Networking.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Networking : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private string Version = "1.0";
+    [SerializeField] [Range(2, 20)] private byte maxPlayers = 2;
+    [SerializeField] private GameObject player;
+    [SerializeField] private Transform spawnPoint;
+    void Start()
+    {
+        PhotonNetwork.GameVersion = Version;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        PhotonNetwork.CreateRoom(null, new Photon.Realtime.RoomOptions { MaxPlayers = maxPlayers});
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Online/PlayerSync.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerSync : MonoBehaviour
+{
+    //[SerializeField] private GameObject LeftHend;
+    //[SerializeField] private GameObject RightHend;
+
+    private void Start()
+    {
+        
+    }
+}
