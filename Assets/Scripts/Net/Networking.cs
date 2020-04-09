4a26a10e79262dd394cc2c2d9a95d99921bceb77
Mixail <mixail.korchikov@mail.ru>
2020-04-09 14:56:00 +0300

    Sync player transform and cube transform
    Синхронизация позиции игрока и кривая синхронизация позиции куба. Два игрока на карте расположены сразу (кривая реализация но будет работать).

---
+++ Assets/Scripts/Net/Networking.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Networking : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private string Version = "1.0";
+    [SerializeField] [Range(2, 20)] private byte maxPlayers = 2;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private GameObject MasterPlayer;
+    [SerializeField] private GameObject Player;
+    void Start()
+    {
+        PhotonNetwork.GameVersion = Version;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        PhotonNetwork.CreateRoom(null, new Photon.Realtime.RoomOptions { MaxPlayers = maxPlayers });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Player.GetComponent<PlayerOnline>().Disable();
+            MasterPlayer.GetComponent<PlayerOnline>().isLocal();
+        }
+        else
+        {
+            Player.GetComponent<PlayerOnline>().isLocal();
+            MasterPlayer.GetComponent<PlayerOnline>().Disable();
+        }
+    }
+}
