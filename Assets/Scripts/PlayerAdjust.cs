72570e3fa2f19ea046f78faba24b370ab4f5c25e
SirotenkoMax <sirotenko.max1998@gmail.com>
2020-07-02 01:32:56 +0300

    Changable player size

--- Assets/Scripts/PlayerAdjust.cs
+++ Assets/Scripts/PlayerAdjust.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
+
 
 public class PlayerAdjust : MonoBehaviour
 {
+    [SerializeField] Transform VRCamera;
+    [SerializeField] SteamVR_Action_Boolean action;
+    [SerializeField] SteamVR_Input_Sources source;
+    [SerializeField] List<Transform> ChangeSize;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,15 @@ public class PlayerAdjust : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (action.GetStateDown(source))
+        {
+            float multiplayer = VRCamera.localPosition.y / 1.7f;
+            foreach(Transform transform in ChangeSize)
+            {
+                transform.localScale = new Vector3(multiplayer, multiplayer, multiplayer);
+
+            }
+        }
+
     }
 }
--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean ChangeSize
+        {
+            get
+            {
+                return SteamVR_Actions.default_ChangeSize;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
