85bc9585eec3d19de566f6051d095ba88bdc0bfd
Mixail <mixail.korchikov@mail.ru>
2020-07-14 20:01:42 +0300

    NEW user sync
    Полностью переделал синхронизацию тел пользователей. Убрал свой менеджер пользователей. Теперь тела удаляются после выхода.

---
+++ Assets/Scripts/Net/LocalPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System;
+
+public class LocalPlayer : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject onlineBodyPref;     //Префаб онлайн тела.
+    [SerializeField] private Transform[] bodyPoints;    //Точки тела, которые нужно синхронизировать с оналйн телом.
+    private Transform transform;
+    private OnlinePlayer onlinePlayer = null;
+
+    private void Awake()
+    {
+        transform = gameObject.transform;
+    }
+
+    public override void OnJoinedRoom() //При входе, создаёт своё тело и запоминает его, что бы менять ему положение.
+    {
+        GameObject onlineBody = PhotonNetwork.Instantiate(onlineBodyPref.name, transform.position, Quaternion.identity);
+        onlinePlayer = onlineBody.GetComponent<OnlinePlayer>();
+        onlinePlayer.hideBody();
+    }
+
+    private void Update()   //Меняет положение своего тела, которое отрпавляеться всем остальным.
+    {
+        if (PhotonNetwork.IsConnected && onlinePlayer != null)
+        {
+            for (int i = 0; i < bodyPoints.Length; ++i)
+            {
+                onlinePlayer.SetTransform(bodyPoints[i], i);
+            }
+        }
+    }
+}
