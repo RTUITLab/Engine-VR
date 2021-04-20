a2a4613284815d92005b3fd2dfeef82ae98a6b95
IvLaptev <ivlaptev13@ya.ru>
2021-04-20 20:47:02 +0300

    Add UI Pointer and VR button

---
+++ Assets/Scripts/MainMenu/VRButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Collider))]
+
+public class VRButton : MonoBehaviour
+{
+    public UnityEvent EventClick;
+    public UnityEvent EventInside;
+    public UnityEvent EventOutside;
+}
