5154e480c563bf133db9e88a4eda0bb06765a63b
Eloren1 <elorenn@bk.ru>
2021-04-20 19:02:40 +0300

    Add VR keys asset

---
+++ Assets/Scripts/MainMenu/NicknameChoose.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NicknameChoose : MonoBehaviour
+{
+    [Header("Позволяет сбрасывать никнейм каждый раз при входе в главное меню." +
+        "Полезно на демонстрационных стендах, чтобы не сохранять чужие ники.")]
+    [SerializeField] private bool resetNicknameAfterRestart = true;
+    private string defaultName = "Сборщик";
+    private string nickname;
+
+    private void Start()
+    {
+        string savedNickname = PlayerPrefs.GetString("Nickname");
+
+        LoadNickname();
+    }
+
+    private void LoadNickname()
+    {
+        if (resetNicknameAfterRestart)
+        { // См. описание флага
+            nickname = RandomNickname();
+            SaveNickname();
+        }
+        else if (PlayerPrefs.HasKey("Nickname"))
+        { // Загружаем сохраненный никнейм
+            nickname = PlayerPrefs.GetString("Nickname");
+        }
+        else // Ни один никнейм пока не был сохранен
+        {
+            nickname = RandomNickname();
+            SaveNickname();
+        }
+    }
+
+    private string RandomNickname()
+    {
+        return defaultName + Random.Range(1000, 10000);
+    }
+
+    public void SaveNickname()
+    {
+        PlayerPrefs.SetString("Nickname", nickname);
+    }
+}
