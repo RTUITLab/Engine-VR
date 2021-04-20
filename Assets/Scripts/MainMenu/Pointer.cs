a96ca791a12695172f64a68c0e07f883fdcceaaa
IvLaptev <ivlaptev13@ya.ru>
2021-04-20 19:28:32 +0300

    Keyboard Pointer

---
+++ Assets/Scripts/MainMenu/Pointer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void UpdateLine()
+    {
+
+    }
+
+ /*   private RaycastHit CreateRaycast(float length)
+    {
+        RaycastHit hit;
+
+        return hit;
+    }*/
+}
