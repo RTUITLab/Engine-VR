a1120e177e2c21aa6137ad0892c06557e7b61e04
IvLaptev <ivlaptev13@ya.ru>
2021-04-21 17:04:56 +0300

    #3: Auto stash before merge of "Issue#3" and "origin/Issue#4"

--- Assets/Scripts/MainMenu/MainMenuController.cs
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -2,31 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using Valve.VR.InteractionSystem;
 
 public class MainMenuController : MonoBehaviour
 {
-    [SerializeField] private Transform target;
-    [SerializeField] private GameObject canvasCenter;
-    private Vector3 position;
 
-    private void LateUpdate()
+    [SerializeField] private Networking networking;
+
+    private void Start()
     {
+<<<<<<< Updated upstream
         // Центруем интерфейс относительно положения игрока
         position = target.transform.position;
         position.y = -0.83f;
         canvasCenter.transform.position = position;
 
         // test
+=======
+        
+>>>>>>> Stashed changes
     }
 
     public void JoinRandomRoom()
     {
         SceneManager.LoadScene(1);
-
-        Destroy(FindObjectOfType<Player>());
     }
 
+    public void OpenList()
+    {
+
+    }
+
+    public void CreateRoom()
+    {
+    }
     // TODO Реализовать остальные методы из меню и навесить их на кнопки
 
     // Нужно сохранять выбор (случайная комната, определенная комната), и при переходе на новую сцену
