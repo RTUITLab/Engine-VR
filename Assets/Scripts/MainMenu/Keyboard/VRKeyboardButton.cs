f0b80d85370d3c217736d96ed7fa56415d664e48
Eloren1 <elorenn@bk.ru>
2021-04-21 17:42:35 +0300

    Continue merging 2 scenes

--- Assets/Scripts/MainMenu/Keyboard/VRKeyboardButton.cs
+++ Assets/Scripts/MainMenu/Keyboard/VRKeyboardButton.cs
@@ -14,6 +14,8 @@ public class VRKeyboardButton : VRButton
         {
             Debug.LogError($"Клавиатура не найдена! {gameObject.name}");
         }
+
+        EventClick.AddListener(KeyClicked);
     }
 
     public void KeyClicked()
