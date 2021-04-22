a7cf3a465b3d456a6564ecfc4085500574d2e53a
Eloren1 <elorenn@bk.ru>
2021-04-22 11:00:29 +0300

    Start making build progress output

---
+++ Assets/Scripts/BuildProgress.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildProgress : MonoBehaviour
+{
+    [SerializeField] private Text progressOutput;
+    [SerializeField] private Slider progressVisual;
+    [SerializeField] private Text currentStageOutput;
+
+    public void ChangeData(int partsCount, // Общее кол-во необходимых деталей
+        int placedPartsCount, // Общее кол-во поставленных деталей
+        int currentStage, // Номер текущей стадии (этапа)
+        int currentPartsCount, // Кол-во необходимых деталей на текущем этапе
+        int currentPlacedPartsCount) // Кол-во поставленных деталей на текущем этапе
+    {
+        progressOutput.text = $"СОБРАНО {placedPartsCount} ДЕТАЛЕЙ ИЗ {partsCount}";
+
+        progressVisual.value = ((float)placedPartsCount / partsCount);
+
+        currentStageOutput.text = $"ЭТАП: {currentStage} — {currentPlacedPartsCount} ДЕТАЛЕЙ ИЗ {currentPartsCount}";
+    }
+}
---
+++ Assets/Scripts/Nickname.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nickname : MonoBehaviour
+{
+    [SerializeField] private Transform nickname;
+
+    private void LateUpdate()
+    {
+        //// Поворачиваем все никнеймы в сторону камеры
+        //transform.LookAt(Camera.main.transform.position);
+        //// nickname.eulerAngles += new Vector3(90, -90, 0);
+
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        //// Никнейм всегда перпендикулярен поверхности
+        var rotation = nickname.rotation;
+        rotation.y = 0;
+        rotation.z = 0;
+        nickname.rotation = rotation;
+    }
+}
