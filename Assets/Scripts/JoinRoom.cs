06c724dadba84e0f82f194ed1c6407319a349236
IvLaptev <ivlaptev13@ya.ru>
2021-04-26 19:37:05 +0300

    Add joining room by name

---
+++ Assets/Scripts/JoinRoom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JoinRoom : MonoBehaviour
+{
+    [SerializeField] private Text text;
+    private Networking networking;
+    // Start is called before the first frame update
+    void Start()
+    {
+        networking = FindObjectOfType<Networking>();
+    }
+
+    public void JoinChosenRoom()
+    {
+        networking.JoinRoom(text.text);
+    }
+}
