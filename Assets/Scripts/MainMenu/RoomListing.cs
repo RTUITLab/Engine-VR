f051fc55b660101064f5e7db99040047c4abd0eb
IvLaptev <ivlaptev13@ya.ru>
2021-04-26 20:09:22 +0300

    Add creating room

--- Assets/Scripts/MainMenu/RoomListing.cs
+++ Assets/Scripts/MainMenu/RoomListing.cs
@@ -8,10 +8,22 @@ using UnityEngine.UI;
 public class RoomListing : MonoBehaviour
 {
     [SerializeField] private Text text;
+    private Networking networking;
     public RoomInfo RoomInfo { get; private set; }
+
+    private void Start()
+    {
+        networking = FindObjectOfType<Networking>();
+    }
+
     public void SetRoomInfo(RoomInfo roominfo)
     {
         RoomInfo = roominfo;
         text.text = roominfo.Name;
+    }
+    public void JoinChosenRoom()
+    {
+        Debug.LogError("Pressed");
+        networking.JoinRoom(text.text);
     }
 }
