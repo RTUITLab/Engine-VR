97a82ef50f95f863793183ff1bf4e54ac8333c6e
Eloren1 <elorenn@bk.ru>
2021-09-28 21:28:30 +0300

    Add shortcut to restart application (Left CTRL + R)

---
+++ Assets/Scripts/GlobalControls.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalControls : MonoBehaviour
+{
+    private bool restartShortcut;
+
+    private void LateUpdate()
+    {
+        restartShortcut = Input.GetKeyDown(KeyCode.R) && Input.GetKey(KeyCode.LeftControl);
+        if (restartShortcut)
+        {
+            RestartApp();
+        }
+    }
+
+    public void RestartApp()
+    {
+        System.Diagnostics.Process.Start(Application.dataPath.Replace("_Data", ".exe"));
+        Application.Quit();
+    }
+}
