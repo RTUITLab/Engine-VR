1bb243cc506c41c81591ad5034f18e156fe29f13
Eloren1 <elorenn@bk.ru>
2021-09-29 14:04:02 +0300

    Add loading screen

---
+++ Assets/Scripts/MainMenu/LoadingScreen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+
+    private void Start()
+    {
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    private IEnumerator LoadSceneAsync()
+    {
+        yield return null;
+
+        AsyncOperation async = SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+        async.allowSceneActivation = false;
+
+        while (async.progress < 0.9f)
+        {
+            slider.value = async.progress;
+        }
+
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+
+        async.allowSceneActivation = true;
+    }
+}
